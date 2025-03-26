cf3b008f8198d7fa64fdff483c52f6db8e974639
Cyprus777 <133809276+Cyprus777@users.noreply.github.com>
2025-03-26 17:42:02 +0400

    Update main.js

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -1,4 +1,5 @@
 (function () {
+    //Бургер----------------------------------------------------------------------------------
     function burgerInit(event) {
         const target = event.target;
         const burgerIcon = target.closest('.burger-icon');
@@ -7,10 +8,6 @@
 
         if (document.documentElement.clientWidth > 900) return
         if (!burgerIcon && !burgerNavLink) return
-
-        // if(burgerIcon) {
-        //     event.preventDefault();
-        // }
 
         if (!document.body.classList.contains('body--opened-menu')) {
             document.body.classList.add('body--opened-menu');
@@ -21,6 +18,7 @@
     }
     document.addEventListener('click', burgerInit)
 
+    //Модалка----------------------------------------------------------------------------------
     const modal = document.querySelector('.modal');
     const modalButton = document.querySelector('.about__img-button');
 
@@ -31,10 +29,72 @@
     function closeModal(event) {
         event.preventDefault();
         const target = event.target;
-        if(target.closest('.modal__cancel') || target.classList.contains('modal')) {
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
             document.body.classList.remove('body--opened-modal');
         };
     };
     modal.addEventListener('click', closeModal);
     modalButton.addEventListener('click', openModal);
+
+    //Табы-----------------------------------------------------------------------------------
+    const tabControls = document.querySelector('.tab__controls');
+    function toggleTab(event) {
+        const tabControl = event.target.closest('.tab__controls-link');
+
+        if (!tabControl) {
+            return
+        }
+        event.preventDefault();
+        if (tabControl.classList.contains('tab__controls-link--active')) {
+            return
+        }
+
+        const tabContentId = tabControl.getAttribute('href');
+        const tabContent = document.querySelector(tabContentId)
+        const activeControl = document.querySelector('.tab__controls-link--active')
+        const activeContent = document.querySelector('.tab-content--show')
+
+        if (activeControl) {
+            activeControl.classList.remove('tab__controls-link--active');
+        }
+        if (activeContent) {
+            activeContent.classList.remove('tab-content--show')
+        }
+
+        tabControl.classList.add('tab__controls-link--active')
+        tabContent.classList.add('tab-content--show')
+    }
+
+    tabControls.addEventListener('click', toggleTab);
+
+    //Аккордион------------------------------------------------------------------------------------
+    const accordionLists = document.querySelectorAll('.accordion-list');
+
+    accordionLists.forEach(element => {
+        element.addEventListener('click', event => {
+
+            const accordionList = event.currentTarget;
+            const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')//
+            const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__control');
+
+            const accordionControl = event.target.closest('.accordion-list__control');
+            if (!accordionControl) return
+            const accordionItem = accordionControl.parentElement
+            const accordionContent = accordionControl.nextElementSibling;
+
+            if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+                accordionOpenedItem.classList.remove('accordion-list__item--opened');
+                accordionOpenedContent.style.maxHeight = null
+            }
+
+            accordionItem.classList.toggle('accordion-list__item--opened');
+
+            if (accordionItem.classList.contains('accordion-list__item--opened')) {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'
+            } else {
+                accordionContent.style.maxHeight = null
+            }
+        })
+
+    })
 })()
