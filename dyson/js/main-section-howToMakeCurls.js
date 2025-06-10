afea2c22adeba7abbc8996cc4a553030651b4a74
Cyprus777 <133809276+Cyprus777@users.noreply.github.com>
2025-06-10 12:56:12 +0400

    Delete dyson directory

--- dyson/js/main-section-howToMakeCurls.js
+++
@@ -1,9 +0,0 @@
-export function enableLastChildSvg760px() {
-    if (window.innerWidth <= 760) {
-        const firstChildSvg = document.querySelector('.howToMakeCurls__show-img-controls svg:first-child')
-        firstChildSvg.style.display = 'none'
-
-        const lastChildSvg = document.querySelector('.howToMakeCurls__show-img-controls svg:last-child')
-        lastChildSvg.style.display = 'inline'
-    }
-}
